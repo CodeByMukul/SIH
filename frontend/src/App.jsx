806896127942eb1815c5962c436fe682cf8a58a9
CodeByMukul <mukulg626@gmail.com>
2024-08-30 03:02:23 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,42 @@
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Dashboard, HomeLayout,  Login, Logout, Register } from "./pages";
+import { ToastContainer, toast } from 'react-toastify';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "logout",
+        element: <Logout />,
+      }
+    ],
+  },
+]);
+
+function App() {
+
+
+  return (
+    <>
+        <RouterProvider router={router} />
+        <ToastContainer position='top-center' />
+    </>
+  )
+}
+
+export default App
